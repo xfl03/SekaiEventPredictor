4098fe9607dfc9f45dd85e643478e50e5f7218bf
DNARoma <tangpei506@gmail.com>
2021-04-03 21:07:26 +0200

    fix: wrong ranks array, access to minio with headers

--- predictForSekaiViewer.ts
+++ predictForSekaiViewer.ts
@@ -8,10 +8,17 @@ const client = redis.createClient(process.env.REDIS_URL);
 
 async function downloadModel() {
   const response = await axios.get(
-    `https://${process.env.MINIO_END_POINT}/${process.env.MINIO_BUCKET}/predict_models.json`
+    `https://${process.env.MINIO_END_POINT}/${process.env.MINIO_BUCKET}/predict_models.json`,
+    {
+      headers: {
+        "user-agent": "Mozilla",
+      },
+    }
   );
   writeFileSync(
-    process.env.IS_SERVERLESS ? "/tmp/predict_models.json" : "predict_models.json",
+    process.env.IS_SERVERLESS
+      ? "/tmp/predict_models.json"
+      : "predict_models.json",
     JSON.stringify(response.data),
     "utf-8"
   );
